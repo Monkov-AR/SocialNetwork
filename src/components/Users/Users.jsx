777c84bba2b306edfd7a654e2b32372db59ab74b
Monkov-AR <igkorolen@gmail.com>
2023-02-11 04:39:15 +0900

    созданы 2 обертки вокруг Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,48 @@
-import axios from "axios";
 import React from "react";
 import userPhoto from "../../assets/images/NullUser.png"
+import style from "./Users.module.css"
 
-let Users = (props) => {
-    let getUsers = () =>{
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {  
-                props.setUsers(response.data.items)
-    
-            });
-    
-        }
+// КЛАССОВАЯ КОМПОНЕНТА(PS. прям JAVA)
+const Users = (props) => {
+
+    //Math.ceil - округляет в большую сторону типа если количество элемтов не делется ровно на страницы
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
-
-    return (
+    debugger
+    return <div>
+        {/* <button onClick={getUsers}>Get Users</button> */}
         <div>
-            <button onClick = {getUsers}>Get Users</button>
-            {props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} />
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                            : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-                    </div>
-                </span>
-                <span>
-                    <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    <span>
-                        <div>{"u.location.country"}</div>
-
-                    </span>
-                </span>
-            </div>)}
+            {pages.map(p => {
+                return <span className={props.currentPage === p && style.selectedPage} onClick={(e) => { props.onPageChanged(p) }}>{p}</span>
+            })}
         </div>
-    );
+        {props.users.map(u => <div key={u.id}>
+            <span>
+                <div>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto} />
+                </div>
+                <div>
+                    {u.followed
+                        ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
+                        : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                </div>
+            </span>
+            <span>
+                <span>
+                    <div>{u.name}</div>
+                    <div>{u.status}</div>
+                </span>
+                <span>
+                    <div>{"u.location.country"}</div>
+
+                </span>
+            </span>
+        </div>)}
+    </div>
 }
+
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,59 @@
 import React from "react";
 import { connect } from "react-redux";
 import { followActionCreator, setCurrentPageActionCreator, setUsersActionCreator, unfollowActionCreator } from "../../redux/usersReducer";
-import Users from "./UsersC";
 
+import axios from "axios";
+import Users from "./Users";
+
+// смысл в чем 
+// было две обертки вокруг презетационной компоненты Users
+// первая это UsersAPIComponent(в последующемUsersContainer) она нужна для конекта к серверу и получения юзеров
+// вторая обертка сделана через connect и нужна для работы со стором
+// потом они все засунуты в один файл
+
+class UsersContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // тут по идее должен быть запрос к серверу
+        // ну или нет, как выяснится позже
+    }
+    // метод для кнопки в Users'ах которая добавляет юзеров
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                this.props.setUsers(response.data.items)
+
+            });
+
+        }
+    }
+    // дефолтные методы жизненного цикла PS. типа есть у любой компоненты
+    //  на колько понял при создание объекта выполняется mount
+    // если объект умирает то просходит unmount функция 
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+
+        });
+    }
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {  
+                this.props.setUsers(response.data.items)
+    
+            });
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage= {this.props.currentPage}
+        onPageChanged={this.onPageChanged}
+        users={this.props.users}
+        />
+    }    
+}
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
@@ -28,4 +79,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
